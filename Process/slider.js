15e70314ebef4ebf37d29b1f3e1a11586ec26099
Ertersy40 <wonk4040@gmail.com>
2024-09-29 15:55:27 +1000

    fixed slider placement and icon

--- Process/slider.js
+++ Process/slider.js
@@ -2,12 +2,11 @@ let lastKnownScrollPosition = 0;
 let ticking = false;
 
 const MAX_ROTATION = 270;
-const MAX_SCROLL = 817;
+const MAX_SCROLL = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ) - window.innerHeight;
 
 const star = document.querySelector(".Process-hero #slider .star");
 
 const steps = document.querySelectorAll(".steps .step");
-console.log(steps);
 
 // Function to determine if an element's middle is within the viewport range
 function isInViewport(element) {
@@ -16,12 +15,12 @@ function isInViewport(element) {
   const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
   
   return (
-    stepMiddle > viewportHeight * 0 && stepMiddle < viewportHeight * 0.3
+    stepMiddle > viewportHeight * 0 && stepMiddle < viewportHeight * 0.6
   );
 }
 
 function doSomething(scrollPos) {
-  console.log(scrollPos);
+//   console.log(scrollPos);
   star.style.transform = `translateY(${scrollPos}px) rotate(${(scrollPos / MAX_SCROLL) * MAX_ROTATION}deg)`;
 
   let glowingStep = null; // Variable to track which step should glow
